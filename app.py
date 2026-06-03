4942ee4b55f09abf8ef450e996fb395d1b1cc565
HuQinKangbet <185679533+HuQinKangbet@users.noreply.github.com>
2026-06-03 17:44:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "GHCR镜像部署成功"
+    return "GHCR实验部署成功！"
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000)
